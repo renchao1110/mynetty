d774ab3ad9e3fd6469ddc18a767d473147baffc2
renc <583816993@qq.com>
2019-06-14 09:35:08 +0800

    新建项目

---
+++ src/test/java/com/glodon/mynetty/mynetty/MynettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glodon.mynetty.mynetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
